08838302887711053dc0e946c890e5facaa45888
Chris Angelico <rosuav@gmail.com>
2023-11-20 10:14:11 +1100

    Uncook the string to prevent warning

--- copywrong.py
+++ copywrong.py
@@ -49,7 +49,7 @@ $""", re.IGNORECASE | re.VERBOSE | re.DOTALL)
 
 midi_files = re.compile(r"^\s*MIDI\s*files\s*$", re.IGNORECASE)
 
-blank = re.compile("^\s*$")
+blank = re.compile(r"^\s*$")
 
 footer = """<footer class="standalone">
 <p class="copyright"><a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a>
